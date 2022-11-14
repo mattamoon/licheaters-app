b3eda1a6ae6f9befe7c78c80ee269a70293cdc3f
Mat Y <me@matyopp.com>
2022-11-14 15:50:37 -0800

    front end update

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
 from gathercheater.gathercheater import GatherCheater
 from gathercheater.functions import *
 from gathercheater.constants import *
+from flask import Flask, jsonify, url_for, render_template
+from dotenv import load_dotenv
+import requests
+
+# Flask Config
+app = Flask(__name__)
 
 
 def check_user_details():
@@ -13,14 +20,15 @@ def check_user_details():
 
     # Set user details
     lichess_obj.user = 'basilcandle'  # set user to check games for
-    lichess_obj.max_games = 10  # set max amount of games to review
+    lichess_obj.max_games = 100  # set max amount of games to review
     lichess_obj.start = '2022/1/1'  # from YYYY/m/d format
     lichess_obj.end = '2022/12/31'  # to YYYY/m/d format
 
     return lichess_obj
 
 
-def app(licheater):
+def check_cheat():
+    licheater = check_user_details()
     games = licheater.games_by_player_dates(game_dates(licheater.start), game_dates(licheater.end))
     players_from_games = licheater.get_players_from_games(games)
     player_list = list_util(players_from_games, licheater.user)
@@ -40,9 +48,21 @@ def app(licheater):
             licheater.data_list.extend(data)
             licheater.df_index += 1
 
-    lichess.display_data(lichess.data_list)
+            cheat, closed, good = licheater.check_cheaters(licheater.data_list)
+
+        return cheat, closed, good
 
 
-if __name__ == "__main__":
-    lichess = check_user_details()
-    app(lichess)
+# script to get accounts
+test_accounts = ['mat', 'mike', 'roger']
+closed_accounts = ['sandra', 'bullock']
+goods = ['I\'m, the greatest', 'yesterday']
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', tos=test_accounts, closed=closed_accounts, good=goods)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
